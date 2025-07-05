3ef8de6945600f05e1d2d4e2c46490c138be659d
Dasha-Khmylova <d.s.hmylova@students.psu.by>
2025-07-05 19:09:23 +0300

    5.js

--- 5.js
+++ 5.js
@@ -1,3 +1,4 @@
+/*Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.*/
 function pigIt(str) {
     return str.split(' ').map(word => {
         if (/^[^a-zA-Z]+$/.test(word)) {
